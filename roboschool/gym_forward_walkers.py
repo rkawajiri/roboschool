0e3326d64aa1d0c0fd3e6144275b30ba1d4f45b9
Ryoma Kawajiri <kawajiri@mattya.cuma>
2017-07-05 11:06:15 +0900

    Fixed walker observation dimensions

--- roboschool/gym_forward_walkers.py
+++ roboschool/gym_forward_walkers.py
@@ -133,14 +133,14 @@ class RoboschoolForwardWalkersBase(RoboschoolMujocoXmlEnv, SharedMemoryClientEnv
 class RoboschoolHopper(RoboschoolForwardWalkersBase):
     foot_list = ["foot"]
     def __init__(self):
-        RoboschoolForwardWalkersBase.__init__(self, "hopper.xml", "torso", action_dim=3, obs_dim=15, power=0.75)
+        RoboschoolForwardWalkersBase.__init__(self, "hopper.xml", "torso", action_dim=3, obs_dim=36, power=0.75)
     def alive_bonus(self, z, pitch):
         return +1 if z > 0.8 and abs(pitch) < 1.0 else -1
 
 class RoboschoolWalker2d(RoboschoolForwardWalkersBase):
     foot_list = ["foot", "foot_left"]
     def __init__(self):
-        RoboschoolForwardWalkersBase.__init__(self, "walker2d.xml", "torso", action_dim=6, obs_dim=22, power=0.40)
+        RoboschoolForwardWalkersBase.__init__(self, "walker2d.xml", "torso", action_dim=6, obs_dim=64, power=0.40)
     def alive_bonus(self, z, pitch):
         return +1 if z > 0.8 and abs(pitch) < 1.0 else -1
     def robot_specific_reset(self):
